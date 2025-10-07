488fb5a25d6cdbf8019d146d2091325e621cbd59
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-07 19:30:49 -0300

    Create kauan_gramkow_estrutura_sequencial_2.py

---
+++ kauan_gramkow_estrutura_sequencial_2.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]"
+# Desenvolvido por: Kauan Gramkow
+
+numero1 = int(input("Digite um valor: "))
+print(f"O número digitado foi: {numero1}")
