12bf5588f5daf26f15ab500240d1497ef370712d
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-07 19:38:03 -0300

    Create kauan_gramkow_estrutura_sequencial_3.py

---
+++ kauan_gramkow_estrutura_sequencial_3.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça dois números e imprima a soma:
+# Desenvolvido por: Kauan Gramkow
+
+numero1 = int(input("Digite o primeiro valor: "))
+numero2 = int(input("Digite o segundo valor: "))
+soma = numero1 + numero2
+print(f"A soma de {numero1} + {numero2} é = {soma}")
