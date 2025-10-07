af003b5f0a7303d12de294957c649249bc31dd80
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-07 19:48:16 -0300

    Create kauan_gramkow_estrutura_sequencial_5.py

---
+++ kauan_gramkow_estrutura_sequencial_5.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números e imprima a soma:
+# Desenvolvido por: Kauan Gramkow
+
+m = float(input("Digite o valor em metros para converter para centímetros: "))
+cm = m * 100
+print(f"{m}m em cm é = {cm}cm")
