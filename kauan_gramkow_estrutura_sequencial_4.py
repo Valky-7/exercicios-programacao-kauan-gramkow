bcaf9600f277b63dd3b09fa96d4342bfc1d481e6
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-07 19:41:20 -0300

    Create kauan_gramkow_estrutura_sequencial_4.py

---
+++ kauan_gramkow_estrutura_sequencial_4.py
@@ -0,0 +1,9 @@
+# Faça um programa que peça dois números e imprima a soma:
+# Desenvolvido por: Kauan Gramkow
+
+n1 = float(input("Digite a sua primeira nota: "))
+n2 = float(input("Digite a sua segunda nota: "))
+n3 = float(input("Digite a sua terceira nota: "))
+n4 = float(input("Digite a sua quarta nota: "))
+media = (n1 + n2 + n3 + n4) / 4
+print(f"A sua média final é = {media}")
