83a161d466e516d48f87e58e1d6388c685e02d52
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-07 20:23:25 -0300

    alteracao

--- kauan_gramkow_estrutura_decisao_1.py
+++ kauan_gramkow_estrutura_decisao_1.py
@@ -1,3 +1,6 @@
+# Faça um programa que peça dois números e imprima o maior deles.
+# Desenvolvido por: Kauan Gramkow
+
 n1 = int(input("Digite o seu primeiro número: "))
 n2 = int(input("Digite o seu segundo número: "))
 if n1 > n2:
---
+++ kauan_gramkow_estrutura_decisao_2.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+# Desenvolvido por Kauan Gramkow
+
+n1 = int(input("Digite um número: "))
+if n1 < 0:
+    print(f"{n1} é um número negativo.")
+else:
+    print(f"{n1} é um número positivo.")
