b67b65dd01f2ab39e329d5f157c38a586f919cfa
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2025-10-15 21:12:04 -0300

    alterações

--- kauan_gramkow_estrutura_repeticao_3.py
+++ kauan_gramkow_estrutura_repeticao_3.py
@@ -1,13 +1,10 @@
 # Faça um programa que leia 5 números e informe a soma e a média dos números.
 # Desenvolvido por Kauan Gramkow
 
-n1 = int(input("Informe o seu primeiro número: "))
-n2 = int(input("Informe o seu segundo número: "))
-n3 = int(input("Informe o seu terceiro número: "))
-n4 = int(input("Informe o seu quarto número: "))
-n5 = int(input("Informe o seu quinto número: "))
-
-soma = n1 + n2 + n3 + n4 + n5
-media = (n1 + n2 + n3 + n4 + n5) / 5
-print(f"A soma entre {n1}, {n2} , {n3}, {n4} e {n5} é = {soma}")
-print(f"A média entre {n1}, {n2}, {n3}, {n4} e {n5} é = {media}")
+soma = 0
+for i in range(1, 6):
+    numero = int(input(f"Informe o {i}º número: "))
+    soma += numero
+media = soma / 5
+print(f"A soma dos números é = {soma}")
+print(f"A média dos números é = {media}")
